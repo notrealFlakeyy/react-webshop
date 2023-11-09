1d452b7031904ce05b5d21bf213615210f0bba64
notrealFlakeyy <svenslucas@icloud.com>
2023-11-09 19:50:17 +0200

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import LanguageSelector from './LanguageSelector'
 
 
 const navigation = [
-    { name: 'Framsidan', href: '/sv' },
+    { name: 'Etusivu', href: '/sv' },
     { name: 'Kontakt', href: '/Contact' },
     { name: 'Hur bokar man taxi', href: '/Booking' },
     { name: 'FPA-Taxi', href: '/Kelataxi' },
